cfc9e43eee747f90ac4b50bd9a0c169e7754f06e
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-06 10:57:19 +0200

    done

---
+++ Script/code.js
@@ -0,0 +1,10 @@
+let Answer = document.getElementById("Output")
+let Display = document.getElementById("Seconds")
+let ArrayBool = [true, false, true, false, true,]
+
+
+
+console.log(ArrayBool.filter(value => value, true).length)
+
+
+
